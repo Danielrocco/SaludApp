c475df69888f2c88006ea3e6d766b4a7ed7ec22f
Daniel <danielrocco04@gmail.com>
2023-11-07 11:51:55 +0100

    Hoy commitee mi saludApp estoy muy contento porque parece ser que todo esta correcto (me quiero morir)

--- app/src/main/java/com/pmdm/saludapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.pmdm.saludapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +19,20 @@ class MainActivity : AppCompatActivity() {
         btnHello.setOnClickListener {
             val name = etName.toString()
             Log.i("Test","Boton Pulsado")
-            Toast.makeText(this, "Hola $name",Toast.LENGTH_SHORT).show()
+
+            if (name.isNotEmpty()) {
+                Snackbar.make(btnHello, "Hola $name!", Snackbar.LENGTH_SHORT)
+                    .setAnchorView(btnHello)
+                    .setAction("RESET") { etName.setText(null) }
+                    .show()
+
+            } else {
+                Toast.makeText(this, "Hola $name",Toast.LENGTH_SHORT).show()
+            }
+
+            val intentGA = Intent(this, GreetingActivity::class.java)
+            intentGA.putExtra("EXTRA_NAME", name)
+            startActivity(intentGA)
         }
     }
 }
